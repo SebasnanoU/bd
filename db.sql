fbc6a4a0a0cb0370ade1642aa173f0d8a87c2c99
Sebastian Rodriguez <desarrollador2@azsmart.net>
2022-11-22 15:11:43 -0500

    TRIGGERS

--- db.sql
+++ db.sql
@@ -25,9 +25,13 @@ PRIMARY KEY(DNI));
 CREATE TABLE MOVIMIENTOS (
   idMOVIMIENTOS INTEGER   NOT NULL ,
   FECHA   timestamp  NOT NULL ,
-  ID_USER INTEGER   NOT NULL ,
-  NOTA_INI INTEGER   NOT NULL ,
-  NOTA_FIN INTEGER   NOT NULL   ,
+  USER_NAME VARCHAR(40)   NOT NULL ,
+  NOTA1_INI INTEGER   NOT NULL ,
+  NOTA1_FIN INTEGER   NOT NULL   ,
+  NOTA2_INI INTEGER   NOT NULL ,
+  NOTA2_FIN INTEGER   NOT NULL   ,
+  NOTA3_INI INTEGER   NOT NULL ,
+  NOTA3_FIN INTEGER   NOT NULL   ,
 PRIMARY KEY(idMOVIMIENTOS));
 
 CREATE TABLE ASIGNATURA (
@@ -169,31 +173,132 @@ GRANT ALL ON EJEMPLARES TO BIBLIOTECARIO;
 GRANT ALL ON PERSONAS TO BIBLIOTECARIO;
 GRANT ALL ON LIBROS_PRESTADOS TO BIBLIOTECARIO;
 
+SELECT NOMBRE FROM ESTUDIANTES
+INNER JOIN PERSONAS USING(DNI)
 
-CREATE OR REPLACE FUCTION create_user_estudent() RETURNS VOID AS $$
-DECLARE x "NOMBRE" % Rowtype;
-BEGIN
-  FOR x IN SELECT * FROM "PERSONAS"
-  LOOP
-    IF X == 
-	
-	
-CREATE OR REPLACE FUNCTION public.create_databaseuser(
-v_username NAME,
-v_password TEXT)
-RETURNS smallint AS
-$BODY$
+CREATE OR REPLACE FUNCTION create_user_estudiante() RETURNS
+TRIGGER AS $create_user_estudiante$
 DECLARE
+ user_name VARCHAR(30) := (SELECT nombre FROM personas WHERE DNI = NEW.DNI);
 BEGIN
-    EXECUTE FORMAT('CREATE ROLE "%I" LOGIN PASSWORD ''%L''', v_username, v_password);
-    RETURN 1;
-    -- Simple Exception
-EXCEPTION
-    WHEN others THEN
-        RETURN 0;
+EXECUTE 'CREATE USER ' || user_name || ' WITH PASSWORD ''' || user_name || '''';
+EXECUTE 'GRANT ESTUDIANTE TO ' || user_name;
+RETURN NEW;
 END;
-$BODY$
-LANGUAGE plpgsql STRICT VOLATILE SECURITY DEFINER
-COST 100;
+$create_user_estudiante$ LANGUAGE plpgsql;
 
-ALTER FUNCTION public.create_databaseuser(NAME, TEXT) OWNER TO postgres;
+CREATE trigger create_user_estudiante AFTER INSERT ON ESTUDIANTES
+FOR EACH ROW EXECUTE PROCEDURE create_user_estudiante();
+
+CREATE OR REPLACE FUNCTION create_user_profesor() RETURNS 
+TRIGGER AS $create_user_profesor$
+DECLARE
+ user_name VARCHAR(30) := (SELECT nombre FROM personas WHERE dni = NEW.PERSONAS_DNI);
+BEGIN
+EXECUTE 'CREATE USER ' || user_name || ' WITH PASSWORD ''' || user_name || '''';
+EXECUTE 'GRANT ESTUDIANTE TO ' || user_name;
+RETURN NEW;
+END;
+$create_user_profesor$ LANGUAGE plpgsql;
+
+CREATE trigger create_user_profesor AFTER INSERT ON PROFESORES
+FOR EACH ROW EXECUTE PROCEDURE create_user_profesor();
+
+CREATE OR REPLACE FUNCTION tbl_MOVIMIENTOS() RETURNS TRIGGER AS $tbl_MOVIMIENTOS$
+DECLARE
+  fecha TIMESTAMP WITH TIME ZONE default NOW();
+  NOTA1 FLOAT := (SELECT nota1 FROM estudiantes_has_grupos WHERE cod = NEW.cod);
+  NOTA2 FLOAT := (SELECT nota2 FROM estudiantes_has_grupos WHERE cod = NEW.cod);
+  NOTA3 FLOAT := (SELECT nota3 FROM estudiantes_has_grupos WHERE cod = NEW.cod);
+BEGIN
+  IF (TG_OP = 'UPDATE') THEN
+    NEW.USER_NAME := user;
+    NEW.FECHA := fecha;
+    NEW.NOTA1_INI := NOTA1;
+    NEW.NOTA1_FIN := NEW.NOTA1;
+    NEW.NOTA2_INI := NOTA2;
+    NEW.NOTA2_FIN := NEW.NOTA2;
+    NEW.NOTA3_INI := NOTA3;
+    NEW.NOTA3_FIN := NEW.NOTA3;
+  	RETURN NEW;
+  END IF;
+END;
+$tbl_MOVIMIENTOS$ LANGUAGE plpgsql;
+
+CREATE trigger tbl_MOVIMIENTOS AFTER UPDATE ON MOVIMIENTOS
+FOR EACH ROW EXECUTE PROCEDURE tbl_MOVIMIENTOS();
+
+insert into FULL_UNIVERSIDAD.facultades values (111,'INGENIERIAS');
+insert into FULL_UNIVERSIDAD.facultades values (112,'CIENCIAS ECONOMICAS');
+insert into FULL_UNIVERSIDAD.facultades values (113,'DERECHO');
+
+insert into FULL_UNIVERSIDAD.personas values (101409872,'CAMILO','BERMUDEZ','12/11/1992',3202746,'CARRERA 13');
+insert into FULL_UNIVERSIDAD.personas values (101409873,'FELIPE','QUINTERO','11/03/1994',2743513,'CARRERA 14');
+insert into FULL_UNIVERSIDAD.personas values (101409874,'ROSA','BERMUDEZ','12/05/1994',2743514,'CARRERA 15');
+insert into FULL_UNIVERSIDAD.personas values (101409875,'TOMAS','CARDENAS','15/04/1992',2743515,'CARRERA 16');
+insert into FULL_UNIVERSIDAD.personas values (101409876,'ANDRES','ROSALES','16/02/1994',2743516,'CARRERA 17');
+insert into FULL_UNIVERSIDAD.personas values (101409877,'SEBASTIAN','TORRES','17/12/1992',2743517,'CARRERA 18');
+insert into FULL_UNIVERSIDAD.personas values (101409878,'JOSE','RUIZ','18/11/1992',2743518,'CARRERA 19');
+
+insert into FULL_UNIVERSIDAD.asignatura values (121,'Programación_B',6,3);
+insert into FULL_UNIVERSIDAD.asignatura values (122,'Bases_De_Datos',4,3);
+insert into FULL_UNIVERSIDAD.asignatura values (123,'Ingles_I',3,3);
+insert into FULL_UNIVERSIDAD.asignatura values (124,'Calculo',8,3);
+insert into FULL_UNIVERSIDAD.asignatura values (125,'Algebra_L',4,2);
+insert into FULL_UNIVERSIDAD.asignatura values (126,'Programación_I',4,3);
+insert into FULL_UNIVERSIDAD.asignatura values (127,'Metodos',3,2);
+insert into FULL_UNIVERSIDAD.asignatura values (128,'Ingles_II',3,2);
+insert into FULL_UNIVERSIDAD.asignatura values (129,'Calculo_I',6,3);
+insert into FULL_UNIVERSIDAD.asignatura values (120,'IA',6,2);
+
+insert into FULL_UNIVERSIDAD.carrera values (131,111, 101409873,9876,'SISTEMAS');
+insert into FULL_UNIVERSIDAD.carrera values (132,112, 101409873,8765,'ADMINISTRACION');
+insert into FULL_UNIVERSIDAD.carrera values (133,112, 101409877,5644,'CONTADURIA');
+insert into FULL_UNIVERSIDAD.carrera values (134,113, 101409877,5444,'DERECHO');
+insert into FULL_UNIVERSIDAD.carrera values (135,112, 101409876,6775,'ECONOMIA');
+
+insert into FULL_UNIVERSIDAD.profesores values (141,111,101409873,'INGENIERO SISTEMAS');
+insert into FULL_UNIVERSIDAD.profesores values (142,113,101409877,'ABOGADO');
+insert into FULL_UNIVERSIDAD.profesores values (143,112,101409876,'CONTADOR');
+
+insert into FULL_UNIVERSIDAD.estudiantes values (151,131,101409872);
+insert into FULL_UNIVERSIDAD.estudiantes values (152,132,101409875);
+insert into FULL_UNIVERSIDAD.estudiantes values (153,134,101409874);
+insert into FULL_UNIVERSIDAD.estudiantes values (154,135,101409878);
+
+insert into FULL_UNIVERSIDAD.libros values (161,'ECONOMIA','PLANETA','PRIMERA EDICION');
+insert into FULL_UNIVERSIDAD.libros values (162,'CONTABILIDAD_3','PLANETA','PRIMERA EDICION');
+insert into FULL_UNIVERSIDAD.libros values (163,'INGLES_4','PLANETA','PRIMERA EDICION');
+insert into FULL_UNIVERSIDAD.libros values (164,'MATEMATICAS_4','PLANETA','PRIMERA EDICION');
+insert into FULL_UNIVERSIDAD.libros values (165,'PROGRAMACION_1','PLANETA','PRIMERA EDICION');
+
+insert into FULL_UNIVERSIDAD.ejemplares values (171,161,4);
+insert into FULL_UNIVERSIDAD.ejemplares values (172,162,6);
+insert into FULL_UNIVERSIDAD.ejemplares values (173,163,8);
+insert into FULL_UNIVERSIDAD.ejemplares values (174,165,3);
+insert into FULL_UNIVERSIDAD.ejemplares values (175,164,4);
+
+insert into FULL_UNIVERSIDAD.ejemplares_has_estudiantes values (171,151,'12/11/2022','12/12/2022');
+insert into FULL_UNIVERSIDAD.ejemplares_has_estudiantes values (171,152,'12/11/2022','12/12/2022');
+insert into FULL_UNIVERSIDAD.ejemplares_has_estudiantes values (171,153,'12/11/2022','12/12/2022');
+insert into FULL_UNIVERSIDAD.ejemplares_has_estudiantes values (171,154,'12/11/2022','12/12/2022');
+insert into FULL_UNIVERSIDAD.ejemplares_has_estudiantes values (172,151,'12/11/2022','12/12/2022');
+insert into FULL_UNIVERSIDAD.ejemplares_has_estudiantes values (174,154,'12/11/2022','12/12/2022');
+insert into FULL_UNIVERSIDAD.ejemplares_has_estudiantes values (174,153,'12/11/2022','12/12/2022');
+
+insert into FULL_UNIVERSIDAD.grupos values (181,121);
+insert into FULL_UNIVERSIDAD.grupos values (182,122);
+insert into FULL_UNIVERSIDAD.grupos values (183,123);
+
+insert into FULL_UNIVERSIDAD.estudiantes_has_grupos values (151,181,3,4,4);
+insert into FULL_UNIVERSIDAD.estudiantes_has_grupos values (152,181,3.5,4,4);
+insert into FULL_UNIVERSIDAD.estudiantes_has_grupos values (153,183,3,4.2,4.5);
+insert into FULL_UNIVERSIDAD.estudiantes_has_grupos values (154,183,3.5,4,4);
+
+insert into FULL_UNIVERSIDAD.personas_has_libros values (101409872,164);
+insert into FULL_UNIVERSIDAD.personas_has_libros values (101409873,163);
+insert into FULL_UNIVERSIDAD.personas_has_libros values (101409874,165);
+
+UPDATE FULL_UNIVERSIDAD.estudiantes_has_grupos 
+SET NOTA2 = 3.7
+WHERE COD = 151;
